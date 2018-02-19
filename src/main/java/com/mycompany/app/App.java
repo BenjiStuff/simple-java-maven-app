da3618a7082e1fea747b21b688814c0c107b759f
BenjiStuff <35224765+BenjiStuff@users.noreply.github.com>
2018-02-19 16:52:31 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,18 +4,18 @@ package com.mycompany.app;
  * Hello world!
  */
 public class App
-{
+{ 
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World!"; 
 
-    public App() {}
+    public App() {} 
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
 
-    private final String getMessage() {
+    private final  String getMessage() {
         return message;
-    }
+    }  
 
-}
+} 
